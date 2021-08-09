471a1dd50e8964886ae3cf78b232d50ef922ff9c
rafaelzanirato <84473402+rafaelzanirato@users.noreply.github.com>
2021-08-09 20:49:28 -0300

    Add files via upload

---
+++ rafael_210809.py
@@ -0,0 +1,17 @@
+print("Question 3 **************************************")
+num=[2,4,6,8,10,12]
+print("Question 4 **************************************")
+for item in range(1,11):
+    if item is in num:
+        print(item)
+print("Question 5 **************************************")
+for value in range(1,7):
+    print(value)
+print("Question 6 **************************************")
+for value in range(1,7):
+    if value < 3:
+        print(str(value)+" is smmaler than 5 and is not bigger than 2")
+    elif value < 5:
+        print(str(value)+" is smaller than 5 and is bigger than 2")
+    else:
+        print(str(value)+" is not smaller than 5 and is bigger than 2")
